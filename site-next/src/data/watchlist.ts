8a42f4d857aba7b491d80d79813b58ba7fe0dcd9
HenryLiu <HenryLiu13550060700@sjtu.edu.cn>
2026-07-15 22:09:56 +0800

    Add 2026 movie watchlist

---
+++ site-next/src/data/watchlist.ts
@@ -0,0 +1,11 @@
+export interface WatchedFilm {
+  title: string;
+  reviewId?: string;
+}
+
+export const watchlist2026: WatchedFilm[] = [
+  { title: '河狸变身计划', reviewId: 'Movies/河狸变身计划' },
+  { title: '给阿嬷的情书', reviewId: 'Movies/给阿嬷的情书' },
+  { title: '小黄人与大怪兽' },
+  { title: '三国第一部：争洛阳' },
+];
